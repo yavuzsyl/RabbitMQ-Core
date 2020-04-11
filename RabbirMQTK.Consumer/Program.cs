aba6fbefd7b5eea679294130de9c5631aaa979af
yavuz <yavuz@farmazon.com.tr>
2020-04-12 02:24:27 +0300

    intro to rabbitmq

--- RabbirMQTK.Consumer/Program.cs
+++ RabbirMQTK.Consumer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace RabbirMQTK.Consumer
 {
@@ -6,7 +9,31 @@ namespace RabbirMQTK.Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://jpvhygyn:y-3JUFmkusd-FmLbcKmPCyhapFV2Mx88@orangutan.rmq.cloudamqp.com/jpvhygyn");
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("rabbitWalking", false, false, false, null);
+
+                    //event-in ctor we specify listen to which channel
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    //autoAck specifies that recieved message will be deleted or not after receiving it
+                    channel.BasicConsume("rabbitWalking",true, consumer);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var messagByte = ea.Body;
+                        var message = Encoding.UTF8.GetString(messagByte);
+                        Console.WriteLine($"Message has been received : \"{message}\" ");
+                    };
+                }
+            }
+            Console.WriteLine("Press the key mah man");
+            Console.ReadLine();
         }
     }
 }
--- RabbirMQTK.Publisher/Program.cs
+++ RabbirMQTK.Publisher/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbirMQTK.Publisher
 {
@@ -6,7 +8,30 @@ namespace RabbirMQTK.Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();//to connect to rmq instance
+            factory.Uri = new Uri("amqp://jpvhygyn:y-3JUFmkusd-FmLbcKmPCyhapFV2Mx88@orangutan.rmq.cloudamqp.com/jpvhygyn");//amqp url aka connection string
+
+            //connection
+            using (var connection = factory.CreateConnection())
+            {
+                //chanel
+                using (var channel = connection.CreateModel())
+                {
+                    //queue-durable false in memory true in disk?-exclusive true only one channel false any channel-autodelete to delete queue after all shit s done
+                    channel.QueueDeclare("rabbitWalking", false, false, false, null);
+
+                    string message = "Rabbit has started walking tellem";
+                    var messageAsByte = Encoding.UTF8.GetBytes(message);
+
+                    //2nd overload => if we dont provide exchange default shit gets in business first parameter, routingkey aka queue?
+                    channel.BasicPublish(string.Empty, routingKey: "rabbitWalking", null, messageAsByte);//message in queue
+
+                    Console.WriteLine("Message has been sent");
+
+                }
+            }
+            Console.WriteLine("Press the key mah man");
+            Console.ReadLine();
         }
     }
 }
