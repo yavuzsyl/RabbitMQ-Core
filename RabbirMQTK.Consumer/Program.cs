2c403ed17b3d44bd11c1897b1c86f6e855fb99ed
yavuz <yavuz@farmazon.com.tr>
2020-04-12 23:44:27 +0300

    Work queues

--- RabbirMQTK.Consumer/Program.cs
+++ RabbirMQTK.Consumer/Program.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client.Events;
 using System;
 using System.Text;
+using System.Threading;
 
 namespace RabbirMQTK.Consumer
 {
@@ -11,29 +12,60 @@ namespace RabbirMQTK.Consumer
         {
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqp://jpvhygyn:y-3JUFmkusd-FmLbcKmPCyhapFV2Mx88@orangutan.rmq.cloudamqp.com/jpvhygyn");
+            //factory.HostName = "localhost";
+
 
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.QueueDeclare("rabbitWalking", false, false, false, null);
+
+                    channel.QueueDeclare("rabbitSpeeding", durable: true, false, false, null); //1.message durability
+                    #region comment
+                    //prefetchCount consumer her seferde mesajları tek tek alacak 1 mesaj işlemi bitmeden diğerini almayacak
+                    //global consumer instance sayısına göre false olursa her instance  tek seferde prefetchCount kadar mesaj alır ,
+                    //global true olursa tüm instancelar toplam olarak tek seferde prefetchCount kadar mesaj alır
+                    //wont consume any messages before the current ones(prefetchCount) done successfully
+                    //if global true all conusmer instances takes prefetchCount as 1 instance, if false every consumer instance takes prefetchCount by one's own 
+                    #endregion
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);//fair dipatch
+
+                    Console.WriteLine("waiting for messages");
 
                     //event-in ctor we specify listen to which channel
                     var consumer = new EventingBasicConsumer(channel);
 
+                    #region comment
                     //autoAck specifies that recieved message will be deleted or not after receiving it
-                    channel.BasicConsume("rabbitWalking",true, consumer);
+                    //when false after consuming message succesfully, consumer will decide to delete or keep the message 
+                    //mesajı aldıktan sonra brokera mesaj başarıyla işlendikten sonra silineceği bilgisini biz vereceğizzz
+                    #endregion
+                    channel.BasicConsume("rabbitSpeeding", autoAck: false, consumer);
 
                     consumer.Received += (model, ea) =>
                     {
                         var messagByte = ea.Body;
                         var message = Encoding.UTF8.GetString(messagByte);
                         Console.WriteLine($"Message has been received : \"{message}\" ");
+
+                        //simulating message process
+                        int simulatedProcessTimeForEveryMessage = int.Parse(GetMessage(args));
+                        Thread.Sleep(simulatedProcessTimeForEveryMessage);
+                        Console.WriteLine("Message has been processed");
+
+                        //provding information to broker that it can delete the message now
+                        channel.BasicAck(ea.DeliveryTag, multiple: false);//2.message acknowledgment
+
+
                     };
+                    Console.WriteLine("Press the key mah man");
+                    Console.ReadLine();
                 }
             }
-            Console.WriteLine("Press the key mah man");
-            Console.ReadLine();
+        }
+        private static string GetMessage(string[] args)
+        {
+            return args[0];
         }
     }
 }
--- RabbirMQTK.Publisher/Program.cs
+++ RabbirMQTK.Publisher/Program.cs
@@ -10,28 +10,45 @@ namespace RabbirMQTK.Publisher
         {
             var factory = new ConnectionFactory();//to connect to rmq instance
             factory.Uri = new Uri("amqp://jpvhygyn:y-3JUFmkusd-FmLbcKmPCyhapFV2Mx88@orangutan.rmq.cloudamqp.com/jpvhygyn");//amqp url aka connection string
+            //factory.HostName = "localhost";
 
             //connection
             using (var connection = factory.CreateConnection())
             {
-                //chanel
+                //channel
                 using (var channel = connection.CreateModel())
                 {
+                    #region comment
                     //queue-durable false in memory true in disk?-exclusive true only one channel false any channel-autodelete to delete queue after all shit s done
-                    channel.QueueDeclare("rabbitWalking", false, false, false, null);
+                    //with durable true messages wont be lost when instance restarted 
+                    #endregion
+                    channel.QueueDeclare("rabbitSpeeding", durable: true, false, false, null);//1.message durability
 
-                    string message = "Rabbit has started walking tellem";
-                    var messageAsByte = Encoding.UTF8.GetBytes(message);
 
-                    //2nd overload => if we dont provide exchange default shit gets in business first parameter, routingkey aka queue?
-                    channel.BasicPublish(string.Empty, routingKey: "rabbitWalking", null, messageAsByte);//message in queue
+                    string message = GetMessage(args);
+                    for (int i = 1; i < 11; i++)
+                    {
+                        var messageAsByte = Encoding.UTF8.GetBytes($"{i}-{message}");
+                        
+                        //keeps safe messages when instance down
+                        var properties = channel.CreateBasicProperties();
+                        properties.Persistent = true;
 
-                    Console.WriteLine("Message has been sent");
+                        //2nd overload => if we dont provide exchange default shit gets in business first parameter, routingkey aka queue?
+                        channel.BasicPublish(string.Empty, routingKey: "rabbitSpeeding", properties, messageAsByte);//message in queue
+
+                        Console.WriteLine($"Message has been sent : {i}-{message}");
+                    }
 
                 }
             }
             Console.WriteLine("Press the key mah man");
             Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return args[0];
         }
     }
 }
