7f84eb350545a526727b12c6eb9e9e6f795d954d
yavuz <yavuz@farmazon.com.tr>
2020-05-03 19:16:04 +0300

    Pdf converter publisher & mail

---
+++ Wrd2Pdf.Consumer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Mime;
+using System.IO;
+using System.Net.Mail;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace Wrd2Pdf.Consumer
+{
+    public class Program
+    {
+        private const string MailFrom = "gicirbeymail@gmail.com";
+        private const string pass = "192837eyrt";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static bool SendEmail(string email, MemoryStream memoryStream, string fileName)
+        {
+            try
+            {
+                //memory başlangıç pozisyonu
+                memoryStream.Position = 0;
+                //içerik tipi pdf olarak seçildi
+                ContentType contentType = new ContentType(MediaTypeNames.Application.Pdf);
+
+                //attachment a streamden dosya okunacak
+                Attachment attachment = new Attachment(contentStream: memoryStream, contentType: contentType);
+                attachment.ContentDisposition.FileName = $"{fileName}.pdf";
+                memoryStream.Close();
+
+                SmtpClient smtpClient = new SmtpClient()
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential("Halo", pass)
+                };
+                MailMessage mailMessage = new MailMessage()
+                {
+                    From = new MailAddress(MailFrom),
+                    Subject = "PDF File convert",
+                    Body = "Look attachment",
+                    IsBodyHtml = true,
+                };
+                mailMessage.To.Add(new MailAddress(email));
+                mailMessage.Attachments.Add(attachment);
+
+                smtpClient.Send(mailMessage);
+
+                Console.WriteLine("Mail Send");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.InnerException.Message);
+                return false;
+            }
+
+        }
+    }
+}
---
+++ Wrd2Pdf.Producer/Models/FileMessageQueue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wrd2Pdf.Producer.Models
+{
+    public class FileMessageQueue
+    {
+        public byte[] WordByte { get; set; }
+        public string Email { get; set; }
+        public string FileName { get; set; }
+    }
+}
