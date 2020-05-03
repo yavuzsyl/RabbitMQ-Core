753afea67c00dc531318c19cc9e49695f5d8799e
yavuz <yavuz@farmazon.com.tr>
2020-05-03 21:07:06 +0300

    PDF Consumer

---
+++ Wrd2Pdf.Consumer/FileMessageQueue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wrd2Pdf.Consumer
+{
+    public class FileMessageQueue
+    {
+        public byte[] WordByte { get; set; }
+        public string Email { get; set; }
+        public string FileName { get; set; }
+    }
+}
--- Wrd2Pdf.Consumer/Program.cs
+++ Wrd2Pdf.Consumer/Program.cs
@@ -4,6 +4,13 @@ using System.IO;
 using System.Net.Mail;
 using System.Net;
 using System.Net.Http.Headers;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Spire.Doc;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using System.Reflection;
 
 namespace Wrd2Pdf.Consumer
 {
@@ -11,9 +18,65 @@ namespace Wrd2Pdf.Consumer
     {
         private const string MailFrom = "gicirbeymail@gmail.com";
         private const string pass = "192837eyrt";
+        private const string ConverterRoutingKey = "WordToPdf";
+        private const string ConverterQueue = "File";
+        private const string ConverterDirectExchange = "converter-exchange";
+        private const string rabbitMqConnectionString = "amqp://jpvhygyn:y-3JUFmkusd-FmLbcKmPCyhapFV2Mx88@orangutan.rmq.cloudamqp.com/jpvhygyn";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri(rabbitMqConnectionString);
+            bool result = false;
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("DirectExchange", type: ExchangeType.Direct, durable: true);
+                    //publisher ile queue oluşturduğumuz için burada queue declare etmeye gerek yok
+                    channel.QueueBind(queue: ConverterQueue, exchange: ConverterDirectExchange, routingKey: ConverterRoutingKey, null);
+
+                    //message dispatch
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    channel.BasicConsume(queue: ConverterQueue, autoAck: false, consumer);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        try
+                        {
+                            Console.WriteLine("Message received from queue processing...");
+                            var messageJsonStr = Encoding.UTF8.GetString(ea.Body.ToArray());
+                            var message = JsonConvert.DeserializeObject<FileMessageQueue>(messageJsonStr);
+
+                            Document document = new Document();
+                            document.LoadFromStream(new MemoryStream(message.WordByte), FileFormat.Docx2013);
+
+                            using (MemoryStream ms = new MemoryStream())
+                            {
+                                document.SaveToStream(ms, FileFormat.PDF);
+                                result = SendEmail(message.Email, ms, message.FileName);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        if (result)
+                        {
+                            Console.WriteLine("Message processed succesfully");
+                            //mesaj işlendi mesajı sil broker kardeşe mesaj
+                            channel.BasicAck(ea.DeliveryTag, multiple: false);
+                        }
+
+                    };
+                    Console.WriteLine("Nicely done press to end");
+                    Console.ReadLine();
+                }
+            }
         }
 
         public static bool SendEmail(string email, MemoryStream memoryStream, string fileName)
@@ -28,14 +91,14 @@ namespace Wrd2Pdf.Consumer
                 //attachment a streamden dosya okunacak
                 Attachment attachment = new Attachment(contentStream: memoryStream, contentType: contentType);
                 attachment.ContentDisposition.FileName = $"{fileName}.pdf";
-                memoryStream.Close();
 
                 SmtpClient smtpClient = new SmtpClient()
                 {
+                    UseDefaultCredentials = false,
                     Host = "smtp.gmail.com",
                     Port = 587,
                     EnableSsl = true,
-                    Credentials = new NetworkCredential("Halo", pass)
+                    Credentials = new NetworkCredential(MailFrom, pass)
                 };
                 MailMessage mailMessage = new MailMessage()
                 {
@@ -44,17 +107,17 @@ namespace Wrd2Pdf.Consumer
                     Body = "Look attachment",
                     IsBodyHtml = true,
                 };
-                mailMessage.To.Add(new MailAddress(email));
+                mailMessage.To.Add(email);
                 mailMessage.Attachments.Add(attachment);
 
                 smtpClient.Send(mailMessage);
-
+                memoryStream.Close();
                 Console.WriteLine("Mail Send");
                 return true;
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.InnerException.Message);
+                Console.WriteLine(ex.Message);
                 return false;
             }
 
